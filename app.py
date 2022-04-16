8ca365deb38a2e9756509305d1b3ac30187332d5
ShabarishRamaswamy <shabarishramaswamy@gmail.com>
2022-04-17 00:08:47 +0530

    Now works with backend.

--- app.py
+++ app.py
@@ -4,9 +4,16 @@ Main Entry point of the Application
 
 # Importing Libraries and Files.
 from flask import Flask
+from src.ml.nlp import find_sponsored_segments
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    return "<p>Welcome to SponsEnd.</p>"
+
+
+@app.route("/sponsor")
+def sponsor():
+    find_sponsored_segments.find_sponsored_segments("a. b")
     return "<p>Welcome to SponsEnd.</p>"
